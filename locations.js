6392d44ab7955088ad2eb3a97a4d9390c9738b27
ssammileex <samantha.errico1@marist.edu>
2014-12-08 15:05:04 -0500

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,114 @@
+var PlayerHasJournal = false;
+var PlayerHasPhotograph = false;
+var PlayerHasKeys = false;
+var PlayerHasMatches = false;
+var PlayerHasWood = false;
+
+	function PlayerTakeJournal(){
+		if(currentLocation === 5 && PlayerHasJournal === false){
+			PlayerHasJournal = true;
+			updateText(item[0].notTaken);
+
+		}else if(currentLocation === 5 && PlayerHasJournal === true){
+		updateText(item[0].taken);
+		}
+	}
+
+	function PlayerTakePhotograph(){
+		if(currentLocation === 4 && PlayerHasPhotograph === false){
+			PlayerHasPhotograph = true;
+			updateText(item[1].notTaken);
+
+		}else if(currentLocation === 5 && PlayerHasPhotograph === true){
+			updateText(item[1].taken);
+		}
+	}
+
+	function PlayerTakeKeys(){
+		if(currentLocation === 2 && PlayerHasKeys === false){
+			PlayerHasKeys = true;
+			updateText(item[2].notTaken);
+
+		}else if(currentLocation === 2 && PlayerHasKeys === true){
+			updateText(item[2].taken);
+		}
+	}
+
+	function PlayerTakeMatches(){
+		if(currentLocation === 3 && PlayerHasMatches === false){
+			PlayerHasMatches = true;
+			updateText(item[3].notTaken);
+
+		}else if(currentLocation === 3 && PlayerHasMatches === true){
+			updateText(item[3].taken);
+		}
+	}
+
+	function PlayerTakeWood(){
+		if(currentLocation === 4 && PlayerHasWood === false){
+			PlayerHasWood = true;
+			updateText(item[4].notTaken);
+
+		}else if(currentLocation === 5 && PlayerHasWood === true){
+			updateText(item[4].taken);
+		}
+	}
+
+	function updateText(msg){
+		var text = document.getElementById("main");
+		text.value=msg+"\n"+"\n"+text.value;
+	}
+	//starting location 
+	function Foyer(){
+		updateText(location[0].description);
+		currentLocation=0;
+	}
+
+	//west of foyer
+	function Kitchen(){
+		updateText(location[1].description);
+		currentLocation=1;
+	}
+
+	//north of foyer
+	function Dinning(){
+		updateText(location[2].description);
+		currentLocation=2;
+	}
+
+	//east of foyer
+	function Living(){
+		updateText(location[3].description);
+		currentLocation=3;
+	}
+
+	//north of dinning 
+	function Hallway(){
+		updateText(location[4].description);
+		currentLocation=4;
+	}
+
+	//north of Hallway
+	function Bedroom(){
+		updateText(location[5].description);
+		currentLocation=5;
+	}
+
+	//south of living 
+	function Basement(){
+		updateText(location[6].description);
+		currentLocation=6;
+	}
+
+	//south of foyer
+	function Courtyard(){
+		updateText(location[7].description);
+		currentLocation=7;
+	}
+
+	//west of Courtyard
+	function Shed(){
+		updateText(location[8].description);
+		currentLocation=8;
+	}
+	

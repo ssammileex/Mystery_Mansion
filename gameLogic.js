71419825c8384143dbd2979f402af01430c9c73d
ssammileex <samantha.errico1@marist.edu>
2014-12-17 17:31:41 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,149 +1,19 @@
-var PlayerHasJournal = false;
-var PlayerHasPhotograph = false;
-var PlayerHasKeys = false;
-var PlayerHasMatches = false;
-
-
-	function PlayerTakeJournal(){
-		if(currentLocation === 5 && PlayerHasJournal === false){
-			PlayerHasJournal = true;
-			updateText(item[0].notTaken);
-
-		}else if(currentLocation === 5 && PlayerHasJournal === true){
-		updateText(item[0].taken);
-		}
-	}
-
-	function PlayerTakePhotograph(){
-		if(currentLocation === 4 && PlayerHasPhotograph === false){
-			PlayerHasPhotograph = true;
-			updateText(item[1].notTaken);
-
-		}else if(currentLocation === 4 && PlayerHasPhotograph === true){
-			updateText(item[1].taken);
-		}
-	}
-
-	function PlayerTakeKeys(){
-		if(currentLocation === 2 && PlayerHasKeys === false){
-			PlayerHasKeys = true;
-			updateText(item[2].notTaken);
-
-		}else if(currentLocation === 2 && PlayerHasKeys === true){
-			updateText(item[2].taken);
-		}
-	}
-
-	function PlayerTakeMatches(){
-		if(currentLocation === 3 && PlayerHasMatches === false){
-			PlayerHasMatches = true;
-			updateText(item[3].notTaken);
-
-		}else if(currentLocation === 3 && PlayerHasMatches === true){
-			updateText(item[3].taken);
-		}
-	}
-
-	function Inventory() {
-		if(PlayerHasJournal === true){
-			document.getElementById("inv1").innerHTML="\n"+(item[0].name);
-		}if(PlayerHasPhotograph === true){
-			document.getElementById("inv2").innerHTML="\n"+(item[1].name);
-		}if(PlayerHasKeys === true){
-			document.getElementById("inv3").innerHTML="\n"+(item[2].name);
-		}if(PlayerHasMatches === true){
-			document.getElementById("inv4").innerHTML="\n"+(item[3].name);
-		}
-	}
-	function drop() {
-		var dropInput = document.getElementById("dropInput").value;
-		if(PlayerHasJournal === true && dropInput==="Journal"){
-			document.getElementById("inv1").innerHTML="";
-			PlayerHasJournal=false;
-		}if(PlayerHasPhotograph === true && dropInput==="Photograph"){
-			document.getElementById("inv2").innerHTML="";
-			PlayerHasPhotograph= false;
-		}if(PlayerHasKeys === true && dropInput==="Keys"){
-			document.getElementById("inv3").innerHTML="";
-			PlayerHasKeys = false;
-		}if(PlayerHasMatches === true && dropInput==="Matches"){
-			document.getElementById("inv4").innerHTML="";
-			PlayerHasMatches = false;
-		}
-	}
-	function enterShed() {
-		if(PlayerHasJournal === true && PlayerHasPhotograph === true && PlayerHasKeys === true && PlayerHasMatches === true && currentLocation === 8){
+function init() {
+var playerName = prompt("Welcome my game. What is your name?");
+	locale[0] = new locales("Location 0", "Foyer", "Pushing through a DO NOT ENTER SIGN you and you're friends fine yourselves in a haunted mansion. The foyer is dark and musty. You hear voices coming from all directions of the house.", "none");
+	locale[1] = new locales("Location 1", "Kitchen", "The kitchen looks as if no one ever used it. The table in the center of the room seems to be set for a dinner for two.", "none");
+	locale[2] = new locales("Location 2", "Dinning Room", "The dinning room is extremely large. One wall is completely windows. On the left most wall there is a cabinet full of beautiful china. There seems to be something glowing from the cabinet, you can take it.", "Keys");
+	locale[3] = new locales("Location 3", "Living Room", "There is a huge fireplace. The house is ice cold. You see matches sitting on the table. You can take them for later. You find a ghost sitting in a red chair. The ghost is telling you about his wife and how he cannot find her ghost. Naturally you want to help.", "Matches");
+	locale[4] = new locales("Location 4", "Hallway", "There is a painting hanging on the wall. The painting strikes you as odd and out of place. Behind it you find a safe. In the safe is a photograph of a couple.", "Photograph");
+	locale[5] = new locales("Location 5", "Bedroom", "The hallway led to a single bedroom. It consists of a bed and a nightstand. On the nightstand there is a leather journal. The journal is filled with a love story.", "Journal");
+	locale[6] = new locales("Location 6", "Basement", "The basement is just a cement room. There is nothing down here besides a musty smell.", "none");
+	locale[7] = new locales("Location 7", "Courtyard", "The house has a huge courtyard completely surrounded by trees, and a porch coming off the back of the house. Next to the porch is a pile of wood. Also, in the furthest west corner of the property you see what looks to be a small shed.", "none");
+	locale[8] = new locales("Location 8", "Shed", "The shed appears to be locked. You can hear screaming from inside.", "none");
+	item[0] = new items("Item 0", "Journal", "a journal containing a passcode", "You take the journal", "You already have the journal");
+	item[1] = new items("Item 1", "Photograph", "a photograph of a husband and wife", "You take the photograph", "You already have the photograph");
+	item[2] = new items("Item 2", "Keys", "a set of keys", "You take the keys", "You already have the keys");
+	item[3] = new items("Item 3", "Matches", "a pack of matches", "You take the matches", "You already have the matches");
 	
-		winGame();	
-		}else{
-			updateText("You need to find all the items to enter.");
-				
-		}
-	}
-	function winGame() {
-		var winNum = prompt("The ghost will not let you enter the shed until you guess the right number between 1 and 2. What is the ghost's number?");
-		var win = Math.floor((Math.random() * 2) +1);
-		var numWin = parseInt(winNum);
-		if(win === numWin) {
-			updateText("You have guessed the correct number! The ghost allows you to enter the shed. You reunited the husband and wife ghost after centuries of being apart. The two ghosts both agree to leave the house for good allowing people to refurbish it and finally live inside knowing it is no longer haunted. Congratulations on winning the game. Thank you for playing.");
-		}else{
-			updateText("This is the wrong number. Type Enter to try again.");
-		}
-	}
-	function updateText(msg){
-		var text = document.getElementById("main");
-		text.value=msg+"\n"+"\n"+text.value;
-	}
-	//starting location 
-	function Foyer(){
-		updateText(locale[0].description);
-		currentLocation=0;
-	}
-
-	//west of foyer
-	function Kitchen(){
-		updateText(locale[1].description);
-		currentLocation=1;
-	}
-
-	//north of foyer
-	function Dinning(){
-		updateText(locale[2].description);
-		currentLocation=2;
-	}
-
-	//east of foyer
-	function Living(){
-		updateText(locale[3].description);
-		currentLocation=3;
-	}
-
-	//north of dinning 
-	function Hallway(){
-		updateText(locale[4].description);
-		currentLocation=4;
-	}
-
-	//north of Hallway
-	function Bedroom(){
-		updateText(locale[5].description);
-		currentLocation=5;
-	}
-
-	//south of living 
-	function Basement(){
-		updateText(locale[6].description);
-		currentLocation=6;
-	}
-
-	//south of foyer
-	function Courtyard(){
-		updateText(locale[7].description);
-		currentLocation=7;
-	}
-
-	//west of Courtyard
-	function Shed(){
-		updateText(locale[8].description);
-		currentLocation=8;
-	}
+	Foyer();
+document.getElementById("player").innerHTML = "Welcome, " + playerName + " to the mystery mansion."
+}
